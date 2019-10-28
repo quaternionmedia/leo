8188f33cac33d38dc37e1282c72ac091f478a9a0
Harpo <harpo@harpo.me>
2019-10-28 10:49:07 -0700

    inline AnnControl elements!

--- src/Control.js
+++ src/Control.js
@@ -5,19 +5,20 @@ var Viewer = require("./Viewer");
 var Annotation = require("./Annotation");
 
 function AnnControl(vnode) {
-    return {
-      view: function(vnode) {return m('#ann2', [
-      m('input#strokeColor', {type: 'color', oninput: function(e) {
-        State.strokeColor(e.currentTarget.value)
-      }}),
-      m('input#strokeWidth', {type: 'range', min: 1, max: 50, value: State.strokeWidth(), oninput: function(e) {
-        State.strokeWidth(e.currentTarget.value)
-      }}, ),
-      m('p#strokeWidthText', State.strokeWidth()),
-      m('input#opactiy', {type: 'range', min: 1, max: 100, value: State.opacity(), oninput: function(e) {
-        State.opacity(e.currentTarget.value)
-      }}),
-      m('p#opacityText', State.opacity()),
+  return {
+    view: function(vnode) {
+      return m('span#ann2', [
+        m('input#strokeColor', {type: 'color', oninput: function(e) {
+          State.strokeColor(e.currentTarget.value)
+        }}),
+        m('input#strokeWidth', {type: 'range', min: 1, max: 50, value: State.strokeWidth(), oninput: function(e) {
+          State.strokeWidth(e.currentTarget.value)
+        }}),
+        m('text#strokeWidthText', State.strokeWidth()),
+        m('input#opactiy', {type: 'range', min: 1, max: 100, value: State.opacity(), oninput: function(e) {
+          State.opacity(e.currentTarget.value)
+        }}),
+        m('text#opacityText', State.opacity()),
     ])
   }
 }
@@ -26,20 +27,20 @@ function AnnControl(vnode) {
 module.exports = {
   view: function(vnode) {
     return [
-    m('button#prev', {
-      onclick: function() {
-        Viewer.prevPage();
-    }}, 'prev'),
-    m('button#next', {
-      onclick: function() {
-        Viewer.nextPage();
-    }}, 'next'),
-    m('button#mode', {
-      onclick: function() {
-        State.annMode(!State.annMode());
+      m('button#prev', {
+        onclick: function() {
+          Viewer.prevPage();
+        }}, 'prev'),
+        m('button#next', {
+          onclick: function() {
+            Viewer.nextPage();
+          }}, 'next'),
+          m('button#mode', {
+            onclick: function() {
+              State.annMode(!State.annMode());
+            }
+          }, State.annMode() ? 'annotate' : 'perform'),
+          State.annMode() ? m(AnnControl) : null,
+        ]
       }
-    }, State.annMode() ? 'annotate' : 'perform'),
-    State.annMode() ? m(AnnControl) : null,
-    ]
-  }
-}
+    }
