2421928e9ef5e92c22deeeb660f94efcfeae991f
Harpo <harpo@harpo.me>
2020-02-28 20:22:14 -0800

    added initDB to read ireal link text

--- ireal.py
+++ ireal.py
@@ -1,6 +1,23 @@
 from urllib.parse import unquote
 from pyRealParser import Tune
+from tinydb import TinyDB, Query
 
-with open('jazz.ireal', 'r') as f:
-    jazz = f.read()
-book = Tune.parse_ireal_url(jazz)
+db = TinyDB('db.json')
+
+def initDB():
+    with open('jazz.ireal', 'r') as f:
+        jazz = f.read()
+        book = Tune.parse_ireal_url(jazz)
+    for tune in book:
+        db.insert({
+            'bpm': tune.bpm,
+            'chord_string': tune.chord_string,
+            'comp_style': tune.comp_style,
+            'composer': tune.composer,
+            'key': tune.key,
+            'repeats': tune.repeats,
+            'style': tune.style,
+            'time_signature': tune.time_signature,
+            'title': tune.title,
+            'transpose': tune.transpose,
+            })
