c5dbe92cca4bdbe29043df3fc9729e3f220f2e88
Harpo <harpo@harpo.me>
2023-09-05 19:09:56 -0700

    Migrates to pdm

--- leo.sh
+++ leo.sh
@@ -3,16 +3,16 @@
 if [ $1 = "dev" ]; then
 shift
 echo "Leo! running dev $1"
-docker-compose -f docker-compose.yml -f dev.yml up $1
+docker compose -f docker-compose.yml -f dev.yml up $1
 
 elif [ $1 = "prod" -o $1 = "production" ]; then
 shift
 echo "Leo! running production $1"
-docker-compose -f docker-compose.yml -f production.yml up $1
+docker compose -f docker-compose.yml -f production.yml up $1
 
 elif [ $1 = "init" ]; then
   shift
   echo "Initilizing Leo!"
-  docker-compose -f docker-compose.yml -f dev.yml run dev yarn install
+  docker compose -f docker-compose.yml -f dev.yml run dev yarn install
 
 fi
