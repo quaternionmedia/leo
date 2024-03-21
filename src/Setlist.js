223a483cdca5185c49ee051187328b5a5aa1db96
Harpo <harpo@harpo.me>
2024-03-20 22:33:54 -0700

    Fixes setlist search

--- src/Setlist.js
+++ src/Setlist.js
@@ -1,52 +1,69 @@
 import m from 'mithril'
 
-export const Setlist = ({ state, actions }) =>
+export const MenuIcon = m('i.fa.fa-bars', {})
+
+export const MenuToggle = ({ state, update }) =>
   m(
-    '#setlist.sidenav',
+    '#closeMenu.closebtn',
     {
-      style: {
-        zIndex: state.menuActive ? 2 : -1,
-        width: state.menuActive ? '250px' : '0',
+      onclick: () => {
+        update({
+          menuActive: !state.menuActive,
+        })
       },
     },
-    m('.menu-header', {}, [
-      m(
-        '#closeMenu.closebtn',
-        {
-          onclick: () => {
-            state.menuActive(false)
-          },
-        },
-        'X'
-      ),
-      m('input#search', {
-        type: 'text',
-        placeholder: 'Search',
-        value: state.search,
-        oninput: e => {
-          state.search(e.currentTarget.value)
-        },
-        onbeforeupdate: (vnode, old) => {
-          console.log('before update', vnode, old)
-          return false
-        },
-        oncreate: vnode => {
-          vnode.dom.focus()
-        },
-      }),
-      state.setlist.map(song => {
+    'X'
+  )
+
+export const Search = ({ state, update }) =>
+  m('input#search', {
+    type: 'text',
+    placeholder: 'Search',
+    value: state.search,
+    oninput: e => {
+      update({ search: e.currentTarget.value })
+    },
+    onbeforeupdate: (vnode, old) => {
+      console.log('before update', vnode, old)
+      return false
+    },
+    oncreate: vnode => {
+      vnode.dom.focus()
+    },
+  })
+
+export const Songlist = ({ state, update }) =>
+  m(
+    '.setlist',
+    {},
+    state.setlist
+      .filter(song => song.search(state.search) > -1)
+      .map(song => {
         return m(
           '.setlist-song',
           {
             id: song,
             onclick: () => {
-              state.search('')
-              state.menuActive(false)
-              actions.loadSetlistIndex(state.songbook().indexOf(song))
+              update({
+                search: '',
+                menuActive: false,
+                song: song,
+              })
             },
           },
           song
         )
-      }),
-    ])
+      })
+  )
+
+export const Setlist = ({ state, update }) =>
+  m(
+    `#setlist.sidenav${state.menuActive ? '.menuActive' : ''}`,
+    {},
+    [
+      m('.menu-header', {}),
+      Search({ state, update }),
+      MenuToggle({ state, update }),
+    ],
+    Songlist({ state, update })
   )
