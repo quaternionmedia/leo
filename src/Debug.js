24498e64f12eee2c064f29f966484450174d9515
Cameron West <Cameron.west@hotmail.com>
2024-04-04 03:35:09 -0500

    WIP: Mobile Styling Vert

--- src/Debug.js
+++ src/Debug.js
@@ -1,9 +1,21 @@
-import m from 'mithril' 
-import meiosisTracer from "meiosis-tracer";
-import "./styles/debug.css";
+import m from 'mithril';
+import meiosisTracer from 'meiosis-tracer';
+import './styles/debug.css';
+
+export const DebugDeviceSize = () =>
+  m(
+    `#deviceSize`,
+    `${
+      parseFloat(document.documentElement.style.getPropertyValue('--vh')) * 100
+    } x 
+     ${
+       parseFloat(document.documentElement.style.getPropertyValue('--vw')) * 100
+     }`
+  );
 
 export const DebugToggle = ({ state: { debugActive }, update }) =>
-  m(`#debugNavToggle${debugActive ? '.open' : ''}`,
+  m(
+    `#debugNavToggle${debugActive ? '.open' : ''}`,
     {
       onclick: () => {
         update({
@@ -14,37 +26,41 @@ export const DebugToggle = ({ state: { debugActive }, update }) =>
     m('.bar.b1'),
     m('.bar.b2'),
     m('.bar.b3')
-  )
+  );
 
-export const DebugNav = cell =>
-  m(`#debugNav`,
-    m('#tracerToggle',
+export const DebugNav = (cell) =>
+  m(
+    `#debugNav`,
+    DebugDeviceSize(),
+    m(
+      '#tracerToggle',
       {
         onclick: () => {
-          const tracer = document.getElementById('tracer')
-          tracer.classList.toggle('hide')
-          const button = document.getElementById('tracerStreamHide_0')
-          button.click()
+          const tracer = document.getElementById('tracer');
+          tracer.classList.toggle('hide');
+          const button = document.getElementById('tracerStreamHide_0');
+          button.click();
         },
       },
-      '🀤'//'✒'
+      '🀤' //'✒'
     ),
-    m('#colorToggle',
+    m(
+      '#colorToggle',
       {
         onclick: () => {
-          const tracer = document.getElementById('page')
-          tracer.classList.toggle('debug')
+          const tracer = document.getElementById('page');
+          tracer.classList.toggle('debug');
         },
       },
       '🀦'
-    ),
-  )
+    )
+  );
 
 // Debug
-export const tracer = (cells) => 
+export const tracer = (cells) =>
   meiosisTracer({
-    selector: "#tracer",
+    selector: '#tracer',
     rows: 25,
-    width: "100%",
-    streams: [{ stream: cells, hide: true, label: "Leo" }],
+    width: '100%',
+    streams: [{ stream: cells, hide: true, label: 'Leo' }],
   });
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import m from "mithril";
+import m from 'mithril';
 // var Viewer = require('./Viewer')
 // import { Nav } from './Nav'
 // import Annotation from './Annotation'
-import { Controls, transposeService } from "./Control";
-import { SideNav, MenuToggle } from "./Setlist";
-import { DebugNav, DebugToggle, tracer } from "./Debug";
-import { State } from "./State";
-import { iRealPage } from "./ireal";
-import { meiosisSetup } from "meiosis-setup";
-import ireal from "./static/jazz.ireal";
-import { Playlist, iRealRenderer } from "ireal-renderer";
-import Fuse from "fuse.js";
-import "./styles/style.css";
-import "./styles/tracer.css";
-import "./styles/screens.css";
+import { Controls, transposeService } from './Control';
+import { SideNav, MenuToggle } from './Setlist';
+import { DebugNav, DebugToggle, tracer } from './Debug';
+import { State } from './State';
+import { iRealPage } from './ireal';
+import { meiosisSetup } from 'meiosis-setup';
+import ireal from './static/jazz.ireal';
+import { Playlist, iRealRenderer } from 'ireal-renderer';
+import Fuse from 'fuse.js';
+import './styles/style.css';
+import './styles/tracer.css';
+import './styles/screens.css';
 
 export const playlist = new Playlist(ireal);
 let renderer = new iRealRenderer();
 
 const fuse = new Fuse(playlist.songs, {
-  keys: ["title", "composer"],
+  keys: ['title', 'composer'],
   threshold: 0.3,
   // includeScore: true,
 });
@@ -34,17 +34,19 @@ const initial: State = {
   darkMode: true,
   transpose: 0,
   fuse: fuse,
-  query: "",
+  query: '',
   search_results: playlist.songs,
 };
 
 export const searchService = {
   onchange: (state) => state.query,
   run: ({ state, update }) => {
-    if (state.query === "") {
+    if (state.query === '') {
       return update({ search_results: playlist.songs });
     }
-    update({ search_results: state.fuse.search(state.query).map((s) => s.item) });
+    update({
+      search_results: state.fuse.search(state.query).map((s) => s.item),
+    });
   },
 };
 
@@ -61,7 +63,7 @@ export const Leo = {
   services: [searchService, transposeService, songService],
   view: (cell) => [
     m(
-      "div#ui",
+      'div#ui',
       MenuToggle(cell),
       SideNav(cell),
       Controls(cell),
@@ -85,7 +87,7 @@ export const Leo = {
 // Initialize Meiosis
 const cells = meiosisSetup<State>({ app: Leo });
 
-m.mount(document.getElementById("app"), {
+m.mount(document.getElementById('app'), {
   view: () => Leo.view(cells()),
 });
 
@@ -109,16 +111,22 @@ window.cells = cells;
 
 function adjustForURLBar() {
   // Set a CSS variable on the root element with the current viewport
-  document.documentElement.style.setProperty("--vh", `${window.innerHeight * 0.01}px`);
-  document.documentElement.style.setProperty("--vw", `${window.innerWidth * 0.01}px`);
+  document.documentElement.style.setProperty(
+    '--vh',
+    `${window.innerHeight * 0.01}px`
+  );
+  document.documentElement.style.setProperty(
+    '--vw',
+    `${window.innerWidth * 0.01}px`
+  );
 }
 
 // Consider running on resize or orientation change
 // events to adjust when the URL bar is shown/hidden
-window.addEventListener("resize", adjustForURLBar);
+window.addEventListener('resize', adjustForURLBar);
 
 // Debug
 tracer(cells);
 
 // actions.loadiReal('/ireal')
-console.log("sup!");
+console.log('sup!');
